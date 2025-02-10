0e455227e7faa5aa2e3c43644fee459d58667cd2
andrestovarr44@gmail.com <andrestovarr44@gmail.com>
2025-02-10 08:59:43 -0500

    db-configuration

--- Trinity-Backend-Test/core/settings.py
+++ Trinity-Backend-Test/core/settings.py
@@ -106,12 +106,24 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+} """
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "trinity_bank",
+        "USER": "postgres",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "5432"
+    }
 }
+
 
 
 # Password validation
